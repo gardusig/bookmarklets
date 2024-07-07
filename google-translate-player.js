a656655f4fab5f00e726674641a0f229d3075e83
Gustavo Gardusi <gardusig@Gustavos-Laptop.local>
2024-07-07 17:02:42 -0300

    .

--- google-translate-player.js
+++ google-translate-player.js
@@ -3,13 +3,17 @@ javascript: (function () {
     return new Promise((resolve) => setTimeout(resolve, ms));
   }
 
-  async function startPlayer() {
-    console.log("Starting the player...");
-    document.querySelector(playButtonSelector).click();
-    console.warn("Clicked play button");
+  async function clickPlayStopButton(playButtonSelector) {
+    const playButton = document.querySelector(playButtonSelector);
+    if (playButton) {
+      playButton.click();
+      console.warn("Clicked play button");
+    } else {
+      console.error("Play button not found");
+    }
   }
 
-  function isPlayerPlaying() {
+  function isPlayerPlaying(playButtonSelector) {
     const playButton = document.querySelector(playButtonSelector);
     if (!playButton) {
       console.error("Play button not found");
@@ -34,14 +38,14 @@ javascript: (function () {
   async function moveToNextPage() {
     console.log("Moving to next page...");
     document.querySelector(nextButtonSelector).click();
-    console.warn("Moved to page");
+    console.warn("Moved to next page");
   }
 
-  async function waitForPlayerToStop() {
+  async function waitForPlayerToStop(playButtonSelector) {
     let totalWaitTime = 0;
     let sleepTime = 3000;
     let attempts = 0;
-    while (isPlayerPlaying() && !stopFlag) {
+    while (isPlayerPlaying(playButtonSelector) && !stopFlag) {
       attempts++;
       console.log(
         `[attempt #${attempts}] Total wait time: ${totalWaitTime}. Sleeping for ${
@@ -56,43 +60,64 @@ javascript: (function () {
     }
   }
 
-  function createStopButton() {
+  function createButton(text, top, right) {
     const button = document.createElement("button");
-    button.innerText = "Stop Script";
+    button.innerText = text;
     button.style.position = "fixed";
-    button.style.top = "10px";
-    button.style.right = "10px";
+    button.style.top = top;
+    button.style.right = right;
     button.style.zIndex = "1000";
     document.body.appendChild(button);
     return button;
   }
 
+  function waitForPlayButtonSelection() {
+    return new Promise((resolve) => {
+      playOriginalButton.addEventListener("click", () => {
+        console.warn("Play Original button clicked");
+        resolve(playButtonOriginalSelector);
+      });
+      playTranslatedButton.addEventListener("click", () => {
+        console.warn("Play Translated button clicked");
+        resolve(playButtonTranslatedSelector);
+      });
+    });
+  }
+
   const nextButtonSelector =
     "#yDmH0d > c-wiz > div > div.ToWKne > c-wiz > div.ef1twd > div > div.vG0hre > button:nth-child(2)";
-  const playButtonSelector =
+  const playButtonOriginalSelector =
     "#yDmH0d > c-wiz > div > div.ToWKne > c-wiz > div.OlSOob > c-wiz > div.ccvoYb > div.AxqVh > div.OPPzxe > c-wiz.rm1UF.dHeVVb.UnxENd > div.FFpbKc > div > div.r375lc > div > div.m0Qfkd > span > button";
+  const playButtonTranslatedSelector =
+    "#yDmH0d > c-wiz > div > div.ToWKne > c-wiz > div.OlSOob > c-wiz > div.ccvoYb > div.AxqVh > div.OPPzxe > c-wiz.sciAJc > div > div.usGWQd > div > div.VO9ucd > div.aJIq1d > div.m0Qfkd > span > button";
   const playButtonStateAttr = "aria-label";
   const stopListeningValue = "Stop listening";
   const charCountSelector =
     "#yDmH0d > c-wiz > div > div.ToWKne > c-wiz > div.OlSOob > c-wiz > div.ccvoYb > div.AxqVh > div.OPPzxe > c-wiz.rm1UF.dHeVVb.UnxENd.rBs9Gd > div.FFpbKc > div > span";
 
   let stopFlag = false;
+  let playButtonSelector = playButtonOriginalSelector;
 
-  const stopButton = createStopButton();
-  stopButton.addEventListener("click", () => {
+  const stopButton = createButton("Stop Script", "10px", "10px");
+  const playOriginalButton = createButton("Play Original", "50px", "10px");
+  const playTranslatedButton = createButton("Play Translated", "90px", "10px");
+
+  stopButton.addEventListener("click", async () => {
     stopFlag = true;
     console.warn("Stop button clicked");
+    await clickPlayStopButton(playButtonSelector);
   });
 
   (async function () {
-    await waitForPlayerToStop();
+    const selectedPlayButtonSelector = await waitForPlayButtonSelection();
+    playButtonSelector = selectedPlayButtonSelector;
+    await waitForPlayerToStop(playButtonSelector);
     while (true) {
-      await startPlayer();
+      await clickPlayStopButton(playButtonSelector);
       await sleep(3000);
-      await waitForPlayerToStop();
+      await waitForPlayerToStop(playButtonSelector);
       if (stopFlag) {
         console.warn("Script stopped by user");
-        await startPlayer();
         break;
       }
       if (!shouldMoveToNextPage()) {
@@ -103,5 +128,7 @@ javascript: (function () {
     }
     console.warn("Finished playing");
     stopButton.remove();
+    playOriginalButton.remove();
+    playTranslatedButton.remove();
   })();
 })();
