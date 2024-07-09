695c38363e1a2f84a51c55d51ab8390325ccf61c
Gustavo Gardusi <gardusig@Gustavos-Laptop.local>
2024-07-09 13:35:05 -0300

    .

--- lottery/dupla-sena.js
+++ lottery/dupla-sena.js
@@ -6,18 +6,21 @@
     document.body.appendChild(script);
   }
 
-  loadScript("https://gardusig.github.io/bookmarklet/", function () {
-    if (
-      typeof DuplaSena !== "undefined" &&
-      typeof DuplaSena.runContestsFromInput === "function" &&
-      typeof DuplaSena.proceedToPayment === "function"
-    ) {
-      (async function () {
-        await DuplaSena.runContestsFromInput();
-        await DuplaSena.proceedToPayment();
-      })();
-    } else {
-      console.error("Failed to load shared library");
+  loadScript(
+    "https://gardusig.github.io/bookmarklet/dupla-sena.js",
+    function () {
+      if (
+        typeof DuplaSena !== "undefined" &&
+        typeof DuplaSena.runContestsFromInput === "function" &&
+        typeof DuplaSena.proceedToPayment === "function"
+      ) {
+        (async function () {
+          await DuplaSena.runContestsFromInput();
+          await DuplaSena.proceedToPayment();
+        })();
+      } else {
+        console.error("Failed to load shared library");
+      }
     }
-  });
+  );
 })();
