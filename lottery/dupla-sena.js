eaf0f6202ccf138a204de05e4957c68cf7b13f42
Gustavo Gardusi <gardusig@Gustavos-Laptop.local>
2024-07-09 12:41:43 -0300

    .

--- lottery/dupla-sena.js
+++ lottery/dupla-sena.js
@@ -3,7 +3,11 @@ javascript: (function () {
     return new Promise((resolve) => setTimeout(resolve, ms));
   }
 
-  async function waitForElement(selector, interval = 1000, timeout = 30000) {
+  function getRandomSleepTime(min = 750, max = 1250) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  async function waitForElement(selector, interval = 100, timeout = 10000) {
     const start = Date.now();
     return new Promise((resolve, reject) => {
       const checkInterval = setInterval(() => {
@@ -23,38 +27,114 @@ javascript: (function () {
     });
   }
 
-  async function clickNumberButtons(numberIds) {
-    for (const id of numberIds) {
-      clickNumberButton(id);
+  async function clickElement(selector) {
+    try {
+      const element = await waitForElement(selector);
+      element.click();
+      await sleep(getRandomSleepTime());
+    } catch (error) {
+      console.error(
+        `Contest #${contestCount}: error clicking element ${selector}:`,
+        error
+      );
     }
   }
 
-  async function clickNumberButton(id) {
+  async function clickNumberButtons(numberIds) {
     try {
-      const numberButton = await waitForElement(`#n${id}`);
-      numberButton.click();
-      console.warn(`Clicked button for number: ${id}`);
-      await sleep(500);
+      let numberPosition = 1;
+      for (const id of numberIds) {
+        const paddedId = id.toString().padStart(2, "0");
+        await clickElement(`#n${paddedId}`);
+        console.log(
+          `Contest #${contestCount}: picked number at position ${numberPosition++}`
+        );
+      }
+      console.warn(`Contest #${contestCount}: finished selecting numbers`);
     } catch (error) {
-      console.error(`Error clicking button for number ${id}:`, error);
+      console.error(
+        `Contest #${contestCount}: error in clickNumberButtons:`,
+        error
+      );
     }
   }
 
   async function addToCart() {
-    try {
-      const addToCartButton = await waitForElement("#colocarnocarrinho");
-      addToCartButton.click();
-      console.warn("Clicked 'Add to Cart' button");
-    } catch (error) {
-      console.error("Error clicking 'Add to Cart' button:", error);
-    }
+    await clickElement("#colocarnocarrinho");
+    console.warn(`Contest #${contestCount}: added to cart`);
   }
 
-  const numberIds = ["01", "02", "03", "04", "05", "06"];
+  async function proceedToPayment() {
+    await clickElement("#irparapagamento");
+    await clickElement("#confirma");
+    console.warn("Payment required");
+  }
+
+  async function processContest(contest) {
+    await clickNumberButtons(contest.numbers);
+    await addToCart();
+    await sleep(getRandomSleepTime(777, 1777));
+  }
+
+  function getContestFromInput(contestCombinationInput) {
+    if (contestCombinationInput.length === 0) {
+      return null;
+    }
+    const numbers = contestCombinationInput
+      .split(",")
+      .map((num) => parseInt(num.trim(), 10));
+    return { numbers: numbers };
+  }
+
+  async function runContestsFromInput() {
+    let input = prompt(
+      "Enter combinations (e.g., 1,2,3,4,5,6;7,8,9,10,11,12):"
+    );
+    if (!input) {
+      input = defaultInput;
+    }
+    const contestCombinations = [];
+    for (const contestCombinationInput of input.split(";")) {
+      const contest = getContestFromInput(contestCombinationInput);
+      if (contest !== null) {
+        contestCombinations.push(contest);
+      }
+    }
+    for (const contest of contestCombinations) {
+      await processContest(contest);
+      contestCount++;
+    }
+    console.warn("All contests processed");
+  }
+
+  let contestCount = 1;
+  const defaultInput =
+    "1,2,3,4,5,6" +
+    ";" +
+    "7,8,9,10,11,12" +
+    ";" +
+    "13,14,15,16,17,18" +
+    ";" +
+    "19,20,21,22,23,24" +
+    ";" +
+    "1,2,3,4,5,7" +
+    ";" +
+    "7,8,9,10,11,13" +
+    ";" +
+    "13,14,15,16,17,19" +
+    ";" +
+    "19,20,21,22,23,25" +
+    ";" +
+    "1,2,3,4,5,8" +
+    ";" +
+    "7,8,9,10,11,14" +
+    ";" +
+    "13,14,15,16,17,20" +
+    ";" +
+    "19,20,21,22,23,26";
 
   (async function () {
-    await clickNumberButtons(numberIds);
-    console.warn("Finished selecting numbers");
-    await addToCart();
+    await runContestsFromInput();
+    await proceedToPayment();
   })();
 })();
